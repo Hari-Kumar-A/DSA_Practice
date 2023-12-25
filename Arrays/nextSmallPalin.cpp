41d9a928105a1356e8c2047e406867f01cd8ade8
harikumar <harikumar3868@gmail.com>
2023-12-25 21:03:21 +0530

    Dec 25 Updated

--- Arrays/nextSmallPalin.cpp
+++ Arrays/nextSmallPalin.cpp
@@ -42,6 +42,27 @@ vector<int> convertToArr(int n) {
     return ans;
 }
 
+void nextPalindrome(vector <int> & digitArr,int len)
+{
+    int left = len % 2 == 0 ? len / 2 -1 : len / 2;
+    int right = len / 2; 
+
+    while (left >= 0 && right < len) {
+        if (digitArr[left] != 9) {
+            digitArr[left]++;
+            if(left!=right)
+            {
+                digitArr[right]++;
+            } 
+            break;
+        } else {
+            digitArr[left] = digitArr[right] = 0;
+            left--;
+            right++;
+        }
+    }
+}
+
 
 
 int main() {
@@ -54,30 +75,85 @@ int main() {
 
     int len = digitArr.size();//length of array
 
-    if (isPalindrome) {
+    if (isPalindrome)
+    {
+
         if (isAll9(digitArr)) {
             cout << n + 2 << endl;//999+2=1001
         } else { 
-            int left = len % 2 == 0 ? len / 2 -1 : len / 2;
-            int right = len / 2; 
+            nextPalindrome(digitArr, len); 
+            printArray(digitArr);
+        }
 
-            while (left >= 0 && right < len) {
-                if (digitArr[left] != 9) {
-                    digitArr[left]++;
-                    if(left!=right)
+
+    } 
+    else
+    {
+
+        int left = (len%2==0)?len / 2 -1 :len / 2; //1 3 | 2 4,left=2, right=3 //1 2 3  4  6 2 8
+        int check=left;
+        int right = len /2;                                 //0 1 2  3  4 5 6  index, n=7
+
+
+        //left=3
+        //right=3
+        //7 8 3   3 2 2
+        bool flag=true;//case 1
+        while(left>=0 && right<len)
+        {
+            if(digitArr[left]==digitArr[right])
+            {
+                left--;
+                right++;
+            }
+            else if(digitArr[left]>digitArr[right])
+            {
+                flag=false;//noraml mirroring
+                break;  
+            }
+            else if(digitArr[left]<digitArr[right])
+            {
+                if(digitArr[left]!=9)//it is 2
+                {
+                    if(left!=check)
                     {
-                        digitArr[right]++;
-                    } 
+                        digitArr[++left]++;
+                    }
+                    else{
+                        digitArr[left]++;
+                    }
+                   
+                    flag=false;
                     break;
-                } else {
-                    digitArr[left] = digitArr[right] = 0;
+                }
+                else // it is 9
+                {
+                    digitArr[left]=0;
                     left--;
                     right++;
                 }
             }
-            printArray(digitArr);
         }
-    }  
+
+            if(flag==false)
+            {
+                left = (len%2==0)?len / 2 -1 :len / 2; //1 3 | 2 4,left=2, right=3 //1 2 3  4  6 2 8
+                right = len /2;  
+                while(left>=0 && right<len)
+                {
+                    digitArr[right]=digitArr[left];
+                    left--;
+                    right++;
+                }
+            }
+             printArray(digitArr); 
+    } 
+    
+
+
+
+   
+   
 
     return 0;
 }
